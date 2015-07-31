1237efb3fc92bd3afa45f2b68d2bc23d6e661759
Anmol Desai <ades597@aucklanduni.ac.nz>
2015-08-01 11:48:23 +1200

    changed the UML and added the robot superclass

---
+++ RobotSuperClass.cpp
@@ -0,0 +1,9 @@
+#include "RobotSuperClass.h"
+#include <iostream>
+
+using namespace std;
+
+RobotSuperClass::RobotSuperClass()
+{
+    cout << "superclass";
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "RobotSuperClass.h"
+
+using namespace std;
+
+int main()
+{
+    RobotSuperClass r;
+
+    return 0;
+}
