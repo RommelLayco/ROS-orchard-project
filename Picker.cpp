879b6fc350404ff542b73aa50c35935bf0ff7bc8
Anmol Desai <ades597@aucklanduni.ac.nz>
2015-08-01 13:23:35 +1200

    Picker class made - ROMMEL MADE THIS

---
+++ Picker.cpp
@@ -0,0 +1,6 @@
+#include "Picker.h"
+
+Picker::Picker()
+{
+    //ctor
+}
---
+++ Picker.h
@@ -0,0 +1,13 @@
+#ifndef PICKER_H
+#define PICKER_H
+
+
+class Picker
+{
+    public:
+        Picker();
+    protected:
+    private:
+};
+
+#endif // PICKER_H
