fc85588ceb8e7829ad44c56a6dfd1ac74b17b14f
xxChinasaurxx <nmol886@auckland.ac.nz>
2015-08-08 15:31:36 +1200

    Beginning of work on collision detection

--- team4_ros/src/mypub.cpp
+++ team4_ros/src/mypub.cpp
@@ -1,32 +1,44 @@
 #include "ros/ros.h" 
-#include "std_msgs/String.h" 
-#include "geometry_msgs/Twist.h"
-#include "live_demo/my_msg.h" 
+#include "sensor_msgs/LaserScan.h" 
+
+void sensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+    ROS_INFO("I heard: [%f]", msg->ranges[0]);
+    int size = 180;//sizeof(msg->ranges)/sizeof(msg->ranges[0]);
+    int i = 0;
+    ROS_INFO("Sensor:");
+    for (i; i < size; i++) {
+         ROS_INFO("I heard: [%f]", msg->ranges[i]);
+    }
+}
+
+
 int main (int argc, char **argv) 
 { 
 	// command line ROS arguments/ name remapping 
 	ros::init(argc, argv, "name_of_mypub_node"); 
 
 	// ROS comms access point 
-	ros::NodeHandle my_handle; 
+	ros::NodeHandle n; 
 
 	// master registry pub/sub 
-	ros::Publisher mypub_object = my_handle.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",100); 
+	//ros::Publisher mypub_object = n.advertise<std_msgs::String>("mypub_topic",100); 
+
+    ros::Subscriber sub = n.subscribe("/robot_0/base_scan", 1000, sensorCallback);
 
 	// loop 10 Hz 
 	ros::Rate loop_rate(10); 
+
+    ros::spin();
         
-        int counter=0;
+    int counter=0;
 	while (ros::ok()) 
 	{ 
 		loop_rate.sleep(); 
-
-		// refer to advertise msg type 
-		  geometry_msgs::Twist mypub_msg; // replace existing similar assignments
-                mypub_msg.linear.x = 1;
-                mypub_msg.linear.y = 1; 
-                //mypub_msg.angular.z = 0.01;
-               mypub_object.publish(mypub_msg);
+        // refer to advertise msg type 
+        //std_msgs::String mypub_msg; 
+        //mypub_msg.data = "hello world"; 
+        //mypub_object.publish(mypub_msg); 
 
 	} 
 
