64fcfb88d523cda1160a76585b403a4ca947fc7f
xxChinasaurxx <nmol886@auckland.ac.nz>
2015-08-08 18:43:12 +1200

    Basic obstacle detection and correction. Worked on by Nick, Rommel, and Anmol.

--- team4_ros/src/mypub.cpp
+++ team4_ros/src/mypub.cpp
@@ -1,45 +1,73 @@
 #include "ros/ros.h" 
-#include "sensor_msgs/LaserScan.h" 
+#include "sensor_msgs/LaserScan.h"
+#include "std_msgs/String.h" 
+#include "geometry_msgs/Twist.h"
+
+ros::Publisher move_pub;
+int x;
+float z;
 
 void sensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    ROS_INFO("I heard: [%f]", msg->ranges[0]);
-    int size = sizeof(msg->ranges)/sizeof(float);
-    ROS_INFO("There are [%i] sensors", size);
     int i = 0;
     ROS_INFO("Sensor:");
-    for (i; i < size; i++) {
-         ROS_INFO("I heard: [%f]", msg->ranges[i]);
+    for (i; i < 180; i++) {
+         if (msg->ranges[i] < 1.5)
+         {
+            ROS_INFO("I'm near something! [%f]", msg->ranges[i]);
+            if (i < 45)
+            {
+                z = 90.0;
+            } else if (i >= 45 && i < 90)
+            {
+                z = 180;
+            } else
+            {
+                z = -90;
+            }
+            
+            
+         }
     }
+}
+
+
+void move()
+{
+    geometry_msgs::Twist move_msg;
+    move_msg.linear.x = x; 
+    move_msg.linear.y = 0; 
+    move_msg.angular.z = z;
+    move_pub.publish(move_msg);
 }
 
 
 int main (int argc, char **argv) 
 { 
 	// command line ROS arguments/ name remapping 
-	ros::init(argc, argv, "name_of_mypub_node"); 
-
+	ros::init(argc, argv, "sensor_node");
+	
 	// ROS comms access point 
-	ros::NodeHandle n; 
+	ros::NodeHandle n;
 
-	// master registry pub/sub 
-	//ros::Publisher mypub_object = n.advertise<std_msgs::String>("mypub_topic",100); 
+    ros::Subscriber sub = n.subscribe("robot_0/base_scan", 1000, sensorCallback);
 
-    ros::Subscriber sub = n.subscribe("/robot_0/base_scan", 1000, sensorCallback);
+    // master registry pub/sub 
+	move_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",100);
 
 	// loop 10 Hz 
-	ros::Rate loop_rate(10); 
-
-    ros::spin();
-        
+	ros::Rate loop_rate(10);
+	
+	x = 1;
+	z = 0;
+	
     int counter=0;
 	while (ros::ok()) 
-	{ 
-		loop_rate.sleep(); 
-        // refer to advertise msg type 
-        //std_msgs::String mypub_msg; 
-        //mypub_msg.data = "hello world"; 
-        //mypub_object.publish(mypub_msg); 
+	{
+	    move();
+	    z = 0;
+		ros::spinOnce();
+		loop_rate.sleep();
 
 	} 
 
