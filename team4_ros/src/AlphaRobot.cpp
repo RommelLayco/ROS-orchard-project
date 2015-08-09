67f07163f0ade1ea888da3daf863d0c0263aba19
Siyang Piao <leonsy5222@gmail.com>
2015-08-09 12:51:38 +1200

    Refine the AlphaRobot node

--- team4_ros/src/AlphaRobot.cpp
+++ team4_ros/src/AlphaRobot.cpp
@@ -19,68 +19,43 @@ geometry_msgs::Twist currentVelocity;
 // Current location of the robot
 geometry_msgs::Pose currentLocation;
 
+// The current angle of the robot
 double currentAngle;
 
-double normalizeAngle(double angle)
-{
-    while (angle < 0) {
-        angle += M_PI;
-    }
-    while (angle > M_PI) {
-        angle -= M_PI;
-    }
-    return angle;
-}
-
-
-bool turnAnticlockwise(double currentAngle, double desiredAngle)
-{   
-    if (currentAngle < 0) {
-        currentAngle = 2 * M_PI + currentAngle;
-    }
-    if (desiredAngle < 0) {
-        desiredAngle = 2 * M_PI + desiredAngle;
-    }
-    desiredAngle = normalizeAngle(desiredAngle - currentAngle);
-    return desiredAngle < M_PI;
-    
-}
 
 void updateCurrentVelocity(){
 
-// Find the correct angle
-    geometry_msgs::Point directionVector; // Vector from currentLocation to desiredLocation
+    // Find the correct angle
+
 
     geometry_msgs::Point desiredLocation;
     desiredLocation.x = -10;
     desiredLocation.y = -21;	
     desiredLocation.z = 0;
 
+    geometry_msgs::Point directionVector; // Vector from currentLocation to desiredLocation
     directionVector.x = desiredLocation.x - currentLocation.position.x;
     directionVector.y = desiredLocation.y - currentLocation.position.y;
     directionVector.z = desiredLocation.z - currentLocation.position.z;
     
-    // Thank god we're only doing 2D stuff
+    // Calculate the desired angle
     double desiredAngle = atan2(directionVector.y, directionVector.x);
-if(desiredAngle == 0)
-{} else
-{       
-    ROS_INFO("currentAngle is : %f",currentAngle); 
-    ROS_INFO("desiredAngle is : %f",desiredAngle); 
 
+// If the desired angle is 0
+if(desiredAngle == 0 || desiredAngle == M_PI)
+{}
+else
+{    
+	   
+    //ROS_INFO("currentAngle is : %f",currentAngle); 
+    //ROS_INFO("desiredAngle is : %f",desiredAngle); 
 
+    // If the deifference between current angle and desired angle is less than 0.1 stop spining
     if (currentAngle-desiredAngle>0.1 || desiredAngle-currentAngle>0.1)
     {
-        // Turn towards angle
+        // Spin
         currentVelocity.linear.x = 0;
-        
-        //if (turnAnticlockwise(currentAngle, desiredAngle)) {
-            // Turn anti clockwise
-            currentVelocity.angular.z = 0.5;
-        //} else {
-            // Turn clockwise
-       
-        //}
+        currentVelocity.angular.z = 0.5;
     } else {
         // Go forward
         currentVelocity.linear.x = 1;
@@ -91,7 +66,7 @@ if(desiredAngle == 0)
 
 
 
-void stageOdometryCallback(const nav_msgs::Odometry msg) 
+void groundTruthCallback(const nav_msgs::Odometry msg) 
 {     
 
 
@@ -105,7 +80,7 @@ void stageOdometryCallback(const nav_msgs::Odometry msg)
     double roll, pitch, yaw;
     tf::Matrix3x3(tf::Quaternion(x, y, z, w)).getRPY(roll, pitch, yaw);
     currentAngle = yaw;
-    ROS_INFO("Yaw is : %f",yaw); 
+    //ROS_INFO("Yaw is : %f",yaw); 
 	
 }
 
@@ -114,17 +89,17 @@ int main (int argc, char **argv)
 	// command line ROS arguments/ name remapping 
 	ros::init(argc, argv, "AlphaRobotNode"); 
 
-	// ROS comms access point 
+	// ROS node hander
 	ros::NodeHandle velPub_handle;
 	ros::NodeHandle sub_handle;  
 
-	// master registry pub 
+	// master registry pub and sub
 	ros::Publisher mypub_object = velPub_handle.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",1000);
 	ros::Subscriber mysub_object;
-	// loop 10 Hz 
+	// loop 25 
 	ros::Rate loop_rate(25);
 
-	mysub_object = sub_handle.subscribe<nav_msgs::Odometry>("robot_0/base_pose_ground_truth",1000, stageOdometryCallback); 
+	mysub_object = sub_handle.subscribe<nav_msgs::Odometry>("robot_0/base_pose_ground_truth",1000, groundTruthCallback); 
 
 	
 
