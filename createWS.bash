495bd7325b414605ab485ae61aa43464821c8450
Rommel <rlay045@aucklanduni.ac.nz>
2015-08-10 15:05:13 +1200

    finished launch file

--- createWS.bash
+++ createWS.bash
@@ -13,10 +13,21 @@ source devel/setup.bash
 
 #move packages to workspace
 cd $DIR
-mv team4_ros ~/ros_WS/src/se306-1
-mv world ~/ros_WS/src/se306-1
+cp -r team4_ros ~/ros_WS/src/se306-1
+cp -r world ~/ros_WS/src/se306-1
 
 #make
 cd ~/ros_WS
 catkin_make
 
+#go to dir and run ros core
+cd ~/ros_WS
+xterm -hold -e roscore&
+
+#open robot nodes
+cd ~/ros_WS/src/se306-1/team4_ros
+xterm -hold -e rosrun team4_ros AlphaWithSensorRobotNode&
+
+#open world file
+cd ~/ros_WS/src/se306-1/world
+xterm -hold -e rosrun stage_ros stageros newworld.world
