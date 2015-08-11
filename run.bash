f646df7239986c67db752718522a5da45dff92f1
Rommel <rlay045@aucklanduni.ac.nz>
2015-08-11 12:07:33 +1200

    add another node to run script

--- run.bash
+++ run.bash
@@ -12,6 +12,7 @@ xterm -hold -e roscore&
 
 cd team4_ros
 xterm -hold -e rosrun team4_ros AlphaWithSensorRobotNode&
+xterm -hold -e rosrun team4_ros DogNode&
 
 cd $DIR/world
 xterm -hold -e rosrun stage_ros stageros newworld.world
