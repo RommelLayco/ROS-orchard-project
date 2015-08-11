60a5e7c9f736115b56a55a1f4dd75d7e35bc3715
Anmol Desai <ades597@aucklanduni.ac.nz>
2015-08-11 17:05:20 +1200

    sample test added. It just needs to access private variables

--- team4_ros/test/AlphaRobotTest.cpp
+++ team4_ros/test/AlphaRobotTest.cpp
@@ -2,6 +2,10 @@
 #include "../src/Robot.h"
 #include "../src/Robot.cpp"
 
+// A hack so that I can access private members
+#define private public
+#define protected public
+
 
 //when they fail, ASSERT_* yields a fatal failure and returns from the current function, 
 //while EXPECT_* yields a nonfatal failure, allowing the function to continue running
@@ -12,6 +16,17 @@ TEST(initialPositionTest, alphaRobotTestCase1){
     // Create a robot
     Robot testRobot = Robot(1, 2, 3);
     ASSERT_EQ(Orienting, testRobot.getState());
+
+}
+
+
+TEST(initialPositionTest, alphaRobotTestCase2){
+    // Create a robot
+    Robot *testRobot = new Robot(1, 2, 3);
+
+    ASSERT_EQ(1,testRobot->current_x_);
+    ASSERT_EQ(1,testRobot->current_y_);
+    ASSERT_EQ(1,testRobot->current_theta_);
 
 }
 
