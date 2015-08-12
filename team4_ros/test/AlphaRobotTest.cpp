c6300bb6fdd2eaf7e7038ae9c1ce66607d88423d
xxChinasaurxx <nmol886@aucklanduni.ac.nz>
2015-08-12 18:15:39 +1200

    renamed testcase names so they are more clear.

--- team4_ros/test/AlphaRobotTest.cpp
+++ team4_ros/test/AlphaRobotTest.cpp
@@ -3,8 +3,6 @@
 #define protected public
 #include "../src/Robot.h"
 #include "../src/Robot.cpp"
-
-
 #include <iostream>
 
 
@@ -15,72 +13,69 @@ using namespace std;
 //Normally, EXPECT_* is the better option since the rest of the test can continue to run and can give useful output.
 //However, ASSERT_* is better if the test shouldn't continue.
 //=================Resident test cases==========================================
-TEST(initialPositionTest, alphaRobotTestCase1){
+TEST(testBasicRobot, testInitialState)
+{
     // Create a robot
     Robot testRobot = Robot(1, 2, 3);
     ASSERT_EQ(Orienting, testRobot.getState());
-
 }
 
 
-TEST(initialPositionTest, alphaRobotTestCase2){
+TEST(testBasicRobot, testInitialPosition)
+{
     // Create a robot
     Robot *testRobot = new Robot(1, 2, 3);
 
-    ASSERT_EQ(1,testRobot->current_x);
-    ASSERT_EQ(2,testRobot->current_y);
-    ASSERT_EQ(3,testRobot->current_theta);
+    ASSERT_EQ(1, testRobot->current_x);
+    ASSERT_EQ(2, testRobot->current_y);
+    ASSERT_EQ(3, testRobot->current_theta);
 }
 
 
-TEST(initialPositionTest,alphaRobotTestCase3){
-
+TEST(testBasicRobot, testLeftCollison)
+{
 	Robot *testRobot = new Robot(1, 2, 3);
 	testRobot->leftCollisionDetected();
 
-	ASSERT_EQ(1,testRobot->linear_velocity_x);
-	ASSERT_EQ(-0.5,testRobot->angular_veloctiy);
-
+	ASSERT_EQ(1, testRobot->linear_velocity_x);
+	ASSERT_EQ(-0.5, testRobot->angular_veloctiy);
 }
 
 
-TEST(initialPositionTest,alphaRobotTestCase4){
-
+TEST(testBasicRobot, testRightCollision)
+{
 	Robot *testRobot = new Robot(1, 2, 3);
 	testRobot->rightCollisionDetected();
 
-	ASSERT_EQ(1,testRobot->linear_velocity_x);
-	ASSERT_EQ(0.5,testRobot->angular_veloctiy);
-
+	ASSERT_EQ(1, testRobot->linear_velocity_x);
+	ASSERT_EQ(0.5, testRobot->angular_veloctiy);
 }
 
-TEST(initialPositionTest,alphaRobotTestCase5){
 
+TEST(testBasicRobot, testCenterCollision)
+{
 	Robot *testRobot = new Robot(1, 2, 3);
 	testRobot->centerCollisionDetected();
 
-	ASSERT_EQ(0,testRobot->linear_velocity_x);
-	ASSERT_EQ(-1.0,testRobot->angular_veloctiy);
-
+	ASSERT_EQ(0, testRobot->linear_velocity_x);
+	ASSERT_EQ(-1.0, testRobot->angular_veloctiy);
 }
 
 
-TEST(reachedLastGoalTest,alphaRobotTestCase6){
-
+TEST(testBasicRobot, testReachedLastGoal)
+{
 	Robot *testRobot = new Robot(1, 2, 3);
-
 	testRobot->goalIndex = 0;
-
 	testRobot->reachedLastGoal();
 
-	ASSERT_EQ(1,testRobot->goalIndex);
-
+	ASSERT_EQ(1, testRobot->goalIndex);
 }
 
 
 
 // Run all the tests that were declared with TEST()
-int main(int argc, char **argv){
-testing::InitGoogleTest(&argc, argv);
-return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
