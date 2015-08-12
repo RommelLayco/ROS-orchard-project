b68b26f417abb3d250f17048b33b6558bc37d243
xxChinasaurxx <nmol886@aucklanduni.ac.nz>
2015-08-12 19:11:07 +1200

    Added two new testcases.

--- team4_ros/test/AlphaRobotTest.cpp
+++ team4_ros/test/AlphaRobotTest.cpp
@@ -31,6 +31,39 @@ TEST(testBasicRobot, testInitialPosition)
     ASSERT_EQ(3, testRobot->current_theta);
 }
 
+TEST(testBasicRobot, testInitalVelocity)
+{
+    Robot *testRobot = new Robot(1, 2, 3);
+
+    ASSERT_EQ(0, testRobot->linear_velocity_x);
+    ASSERT_EQ(0, testRobot->linear_velocity_y);
+    ASSERT_EQ(0, testRobot->angular_veloctiy);
+}
+
+TEST(testBasicRobot, testPositionCallback){
+    Robot *testRobot = new Robot(1, 2, 3);
+
+    // Create Odometry message
+    nav_msgs::Odometry positionMessage;
+    double w = 0.9;
+    double z = 1.5;
+    positionMessage.pose.pose.position.x = 10;
+    positionMessage.pose.pose.position.y = 20;
+    positionMessage.pose.pose.orientation.x = 0;
+    positionMessage.pose.pose.orientation.y = 0;
+    positionMessage.pose.pose.orientation.z = z;
+    positionMessage.pose.pose.orientation.w = w;
+
+    double yaw = tf::getYaw(tf::Quaternion(0, 0, z, w));
+
+    testRobot->positionCallback(positionMessage);
+
+    ASSERT_EQ(10, testRobot->current_x);
+    ASSERT_EQ(20, testRobot->current_y);
+    ASSERT_EQ(yaw, testRobot->current_theta);
+
+
+}
 
 TEST(testBasicRobot, testLeftCollison)
 {
