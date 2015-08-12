65020a9f571798d14f9935362b67acbfb7f9af1e
Siyang Piao <leonsy5222@gmail.com>
2015-08-12 22:39:33 +1200

    Modify the master node to subscribe the bin_topic

--- team4_ros/src/Master.cpp
+++ team4_ros/src/Master.cpp
@@ -7,53 +7,26 @@ ros::Publisher move_pub;
 int x;
 float z;
 
-void sensorCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+void binCallback(const std_msgs::String msg)
 {
-    int i = 0;
-    ROS_INFO("Sensor:");
-    for (i; i < 180; i++) {
-         if (msg->ranges[i] < 5)
-         {
-            ROS_INFO("I'm near something! [%f]", msg->ranges[i]);
-            if (i < 45)
-            {
-                z = 90.0;
-            } else if (i >= 45 && i < 90)
-            {
-                z = 180;
-            } else
-            {
-                z = -90;
-            }
-            
-            
-         }
-    }
+if (msg.data.compare("I AM Full") != 0 )
+
 }
 
 
-void move()
-{
-    geometry_msgs::Twist move_msg;
-    move_msg.linear.x = x; 
-    move_msg.linear.y = 0; 
-    move_msg.angular.z = z;
-    move_pub.publish(move_msg);
-}
 
 
 int main (int argc, char **argv) 
 { 
 	// command line ROS arguments/ name remapping 
-	ros::init(argc, argv, "sensor_node");
+	ros::init(argc, argv, "master_node");
 	
 	// ROS comms access point 
 	ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("robot_0/base_scan", 1000, sensorCallback);
+    ros::Subscriber sub = n.subscribe("bin_topic", 1000, binCallback);
 
     // master registry pub/sub 
-	move_pub = n.advertise<geometry_msgs::Twist>("robot_0/cmd_vel",100);
 
 	// loop 10 Hz 
 	ros::Rate loop_rate(10);
@@ -64,8 +37,7 @@ int main (int argc, char **argv)
     int counter=0;
 	while (ros::ok()) 
 	{
-	    move();
-	    z = 0;
+
 		ros::spinOnce();
 		loop_rate.sleep();
 
